4b3ed91d59a4a3569881e95fbf3d6ba026d90e2b
Shinisuryu <MuaDragon@gmail.com>
2015-12-09 14:46:01 -0800

    Version 1.5
    Added support for items that come in preset stacks and items that don't
    stack at all.
    Limited the maximum you can attempt to buy as described above.

--- BuyEmAll.lua
+++ BuyEmAll.lua
@@ -1,20 +1,22 @@
---[[ BuyEmAll - By Cogwheel v1.0 - See readme.txt
+--[[ BuyEmAll - By Cogwheel v1.5 - See readme.txt
 
-Allows you to buy more than the stack size of an item from a vendor at once. ]]
+Allows you to buy more than the stack size of an item from a vendor at once. 
+Additionally, you can now shift-click to buy multiple units of items that are
+sold in preset stacks (e.g. Refreshing Spring Water)
+
+]]
+
+--[[ Hook BuyMerchantItem for debugging purposes 
+function BuyMerchantItem(button, amount)
+    if not amount then amount = 1 end
+    print("Buying "..amount.." items.")
+end --]]
 
 
-local OldOpenStackSplitFrame = OpenStackSplitFrame
 
-function OpenStackSplitFrame(maxStack, parent, anchor, anchorTo)
-	if string.find(parent:GetName(), "^MerchantItem.-ItemButton$") then
-		BuyEmAll_trueMax = maxStack
-		maxStack = 10000
-	end
-
-	return OldOpenStackSplitFrame(maxStack, parent, anchor, anchorTo)
-end
 
 function BuyEmAll_OnLoad()
+    -- Set up confirmation dialog
 	StaticPopupDialogs["BUYEMALL_CONFIRM"] = {
 		text = "Are you sure you want to buy %d of this item?",
 		button1 = TEXT(YES),
@@ -24,24 +26,136 @@ function BuyEmAll_OnLoad()
 		hideOnEscape = true
 	}
 	
+    -- Replace the SplitStack callback for each merchant item button
 	for i=1,12 do
-		getglobal("MerchantItem"..i.."ItemButton").SplitStack = function(button, split)
-			if (split > 0) then
-				if split > BuyEmAll_trueMax then
-					local dialog = StaticPopup_Show("BUYEMALL_CONFIRM", split)
-					dialog.data = { button = button, amount = split }
-				else
-					BuyMerchantItem(button:GetID(), split);
-				end
-			end
-		end
+		getglobal("MerchantItem"..i.."ItemButton").SplitStack = BuyEmAll_SplitStack
 	end
 end
 
-function BuyEmAll_OnAccept(params)
-	for i=1,math.floor(params.amount / BuyEmAll_trueMax) do
-		BuyMerchantItem(params.button:GetID(), BuyEmAll.trueMax)
+
+
+
+function MerchantItemButton_OnClick(button, ignoreModifiers)
+	if ( MerchantFrame.selectedTab == 1 ) then
+		-- Is merchant frame
+		if ( button == "LeftButton" ) then
+			if ( IsControlKeyDown() and not ignoreModifiers ) then
+				DressUpItemLink(GetMerchantItemLink(this:GetID()));
+			elseif ( IsShiftKeyDown() and not ignoreModifiers ) then
+				if ( ChatFrameEditBox:IsVisible() ) then
+					ChatFrameEditBox:Insert(GetMerchantItemLink(this:GetID()));
+				else
+                    BuyEmAll_ChooseAmount(this)
+				end
+			else
+				PickupMerchantItem(this:GetID());
+			end
+		else
+			if ( IsControlKeyDown() and not ignoreModifiers ) then
+				return;
+			elseif ( IsShiftKeyDown() and not ignoreModifiers ) then
+                BuyEmAll_ChooseAmount(this)
+			else
+				BuyMerchantItem(this:GetID());
+			end
+		end
+	else
+		-- Is buyback item
+		BuybackItem(this:GetID());
 	end
-	local leftover = math.mod(params.amount, BuyEmAll_trueMax)
-	if leftover then BuyMerchantItem(params.button:GetID(), leftover) end
+end
+
+
+
+
+function BuyEmAll_ChooseAmount(this)
+    local name, _, price, quantity, numAvailable = GetMerchantItemInfo(this:GetID());
+    if not name or numAvailable == 0 then
+        return
+    elseif numAvailable == 1 then
+        MerchantItemButton_OnClick("LeftButton", 1)
+        return
+    end
+    local _,_,itemString = string.find(GetMerchantItemLink(this:GetID()), "|H(.-)|h")
+    _,_,_,_,_,_,this.stackSize = GetItemInfo(itemString)
+    this.presetStack = quantity
+
+    local bagMax = math.floor(BuyEmAll_FreeSpace(name, this.stackSize) / quantity) * quantity
+    local moneyMax = math.floor(GetMoney() / price) * quantity
+    local maxPurchase = math.min(bagMax, moneyMax)
+    if numAvailable > 0 then maxPurchase = math.min(maxPurchase, numAvailable * quantity) end
+
+    OpenStackSplitFrame(maxPurchase, this, "BOTTOMLEFT", "TOPLEFT");
+end
+
+
+
+
+function BuyEmAll_FreeSpace(name, stackSize)
+    local freeSpace = 0
+    
+	for theBag = 0,4 do
+		local numSlot = GetContainerNumSlots(theBag);
+		for theSlot = 1, numSlot do
+			local itemLink = GetContainerItemLink(theBag, theSlot);
+			if not itemLink then
+				freeSpace = freeSpace + stackSize;
+			elseif string.find(itemLink, "%["..name.."%]") then
+                local _,itemCount = GetContainerItemInfo(theBag, theSlot)
+                freeSpace = freeSpace + stackSize - itemCount
+            end
+		end
+	end
+    
+    return freeSpace
+end
+
+
+
+
+function BuyEmAll_SplitStack(button, amount)
+    if (amount > 0) then
+        amount = math.ceil(amount/button.presetStack) * button.presetStack
+        local params = { button = button, amount = amount }
+        if amount > button.stackSize then
+            local dialog = StaticPopup_Show("BUYEMALL_CONFIRM", amount)
+            dialog.data = params
+        elseif button.presetStack > 1 then
+            BuyEmAll_OnAccept(params)
+        else
+            BuyMerchantItem(button:GetID(), amount);
+        end
+    end
+end
+
+
+
+
+function BuyEmAll_OnAccept(params)
+    local numLoops, purchAmount, leftover
+    
+    if params.button.presetStack > 1 then
+        numLoops = params.amount/params.button.presetStack
+        purchAmount = 1
+        leftover = 0
+    else
+        numLoops = math.floor(params.amount/params.button.stackSize)
+        purchAmount = params.button.stackSize
+        leftover = math.mod(params.amount, params.button.stackSize)
+    end
+    
+    for i = 1, numLoops do
+        BuyMerchantItem(params.button:GetID(), purchAmount)
+    end
+    
+    if leftover > 0 then BuyMerchantItem(params.button:GetID(), leftover) end
+end
+
+
+
+
+local OldMerchantFrame_OnHide = MerchantFrame_OnHide
+function MerchantFrame_OnHide(...)
+    StaticPopup_Hide("BUYEMALL_CONFIRM")
+    return OldMerchantFrame_OnHide(unpack(arg))
 end
--- readme.txt
+++ readme.txt
@@ -1,12 +1,21 @@
-BuyEmAll - v1.0
+BuyEmAll - v1.5
 
 By Cogwheel
 
-When you shift-click an item at a vendor, you will now be able to buy more than the stack size of the item at once (assuming you have enough money and room in your inventory). It pops up a confirmation box for amounts larger than the stack size to be on the safe side.
+When you shift-click an item at a vendor, this addon allows you to buy more than the stack size of the item at once. It pops up a confirmation box for amounts larger than the stack size to be on the safe side.
+
+The amount you can purchase is limited by:
+
+* The amount of free space in your bags. Partial stacks are included in this calculation, but specialty bags are not (e.g. free slots in herbalism bags will be counted towards your free space regardless of what you're purchasing)
+
+* The amount you can afford
+
+* The number available from the vendor (for limited stock items). If only one item is available, shift-click behaves just like a left click, picking up one of the item onto your cursor.
 
 Please visit Cogwheel's Workshop at http://wowinterface.com/portal.php?&uid=17646 if you have any suggestions or complaints.
 
 
 TODO:
 
-- Add support for items that come in stacks in the first place.
+- Add information and options to the Stack Split window including maximum amount, buy max button, etc.
+- Way down the road... maybe... add a database of items and bag types so that special bag slots are only counted for items that will go into those bags
